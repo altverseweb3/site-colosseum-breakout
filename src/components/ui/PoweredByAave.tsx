8886d9caf4a113ce2437df4c020bbd4f7eae9772
silentMaestro <paulpapas321@gmail.com>
2025-04-19 17:22:10 -0400

    remove comment line

--- src/components/ui/PoweredByAave.tsx
+++ src/components/ui/PoweredByAave.tsx
@@ -6,7 +6,6 @@ const PoweredByAave = () => {
         <div className="flex justify-end mt-2">
             <div className="flex items-center gap-2">
                 <span className="text-sm text-white">powered by</span>
-                {/* Option 1: Use imported SVG */}
                 <img src={`/protocols/aave.svg`} alt="Aave Logo" className="h-3" />
             </div>
         </div>
