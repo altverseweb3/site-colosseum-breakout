e07f23a3c9d85a18b9928671956162b4c431d45d
silentMaestro <paulpapas321@gmail.com>
2025-04-19 22:13:22 -0400

    chore: lint all changes

--- src/app/(dapp)/borrow/page.tsx
+++ src/app/(dapp)/borrow/page.tsx
@@ -1,16 +1,10 @@
 "use client";
 
 import React from "react";
-import { useTokenTransfer } from "@/utils/walletMethods";
-import { TokenTransfer } from "@/components/ui/TokenTransfer";
 import BorrowLend from "@/components/ui/BorrowLend";
 
 const BorrowLendComponent: React.FC = () => {
-
-
-  return (
-    <BorrowLend />
-  );
+  return <BorrowLend />;
 };
 
 export default BorrowLendComponent;
