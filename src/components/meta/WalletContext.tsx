c1478ac1d39b51ee5bad7fa1e404f27090b8a2d3
elmoreshaw <elmoreshaws@gmail.com>
2025-04-30 07:06:00 +1000

    feat: added phantom wallet support

--- src/components/meta/WalletContext.tsx
+++ src/components/meta/WalletContext.tsx
@@ -9,6 +9,7 @@ import {
   base,
   optimism,
   polygon,
+  solana,
 } from "@reown/appkit/networks";
 
 // 1. Get projectId at https://cloud.reown.com
@@ -27,6 +28,7 @@ createAppKit({
   adapters: [new EthersAdapter()],
   includeWalletIds: [
     "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
+    "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393",
   ],
   excludeWalletIds: [
     "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
@@ -35,7 +37,7 @@ createAppKit({
     "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
   ],
   metadata,
-  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon],
+  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon, solana],
   projectId,
   features: {
     analytics: true,
